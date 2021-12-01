8fe22af28a95045c8801169e9d8386d7f98e6614
Sistania Bong <sistaniameilina@gmail.com>
2021-12-01 00:14:37 -0500

    add models

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+  
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+module.exports = { Post, User, Comment };
